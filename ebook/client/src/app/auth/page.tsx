8d29b4064ecab5ebc0058da01d48cdcbe1cabfe3
Aaryan Sharma <144255729+Aaryan10-sudo@users.noreply.github.com>
2025-07-15 17:22:09 +0545

    Delete ebook directory

--- ebook/client/src/app/auth/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthForm from "@/components/auth/AuthForm";
-
-export default function pricing() {
-  return (
-    <div className="">
-      <AuthForm />
-    </div>
-  );
-}
--- ebook/client/src/app/pricing/page.tsx
+++
@@ -1,9 +0,0 @@
-import PricingTable from "@/components/Pricing/PricingTable";
-
-export default function pricing() {
-  return (
-    <div className="">
-      <PricingTable />
-    </div>
-  );
-}
